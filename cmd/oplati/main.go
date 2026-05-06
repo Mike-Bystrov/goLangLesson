36f5e8ed8f89083d5033a3402110d85194195688
mikee <vip.bmv2002@gmail.com>
2026-05-06 23:56:57 +0300

    выполнены следующие пункты  TODO: пополнить баланс     //  TODO: просмотреть информацию о конкретном пользователе по id     //  TODO: снять деньги с баланса в // TODO: доделать public server судя по всему была просто ошибка возвращаемого типа, я запустил теперь public работает

--- cmd/oplati/main.go
+++ cmd/oplati/main.go
@@ -15,7 +15,7 @@ import (
 )
 
 const (
-	publicAddr  = ":8080"
+	publicAddr  = ":8082"
 	privateAddr = ":8081"
 )
 
@@ -30,25 +30,24 @@ func init() {
 func main() {
 	oplatiService := oplati.New(inmemory.NewStorage())
 	// TODO: доделать public server
-	//publicServer := hserver.NewPublicServer(oplatiService, publicAddr, hmiddlewares.LoggingMiddleware)
-	privateServer := hserver.NewPrivateServer(oplatiService, privateAddr, hmiddlewares.LoggingMiddleware)
-
-	/*	go func() {
-			log.Info().Str("addr", publicAddr).Msg("public server started...")
-			err := publicServer.ListenAndServe()
-			if err != nil {
-				log.Err(err).Msg("failed to start public server")
-			}
-		}()
-	*/
+	publicServer := hserver.NewPublicServer(oplatiService, publicAddr, hmiddlewares.LoggingMiddleware)
+	//privateServer := hserver.NewPrivateServer(oplatiService, privateAddr, hmiddlewares.LoggingMiddleware)
 
 	go func() {
-		log.Info().Str("addr", privateAddr).Msg("private server started...")
-		err := privateServer.ListenAndServe()
+		log.Info().Str("addr", publicAddr).Msg("public server started...")
+		err := publicServer.ListenAndServe()
 		if err != nil {
-			log.Err(err).Msg("failed to start private server")
+			log.Err(err).Msg("failed to start public server")
 		}
 	}()
+
+	// go func() {
+	// 	log.Info().Str("addr", privateAddr).Msg("private server started...")
+	// 	err := privateServer.ListenAndServe()
+	// 	if err != nil {
+	// 		log.Err(err).Msg("failed to start private server")
+	// 	}
+	// }()
 
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
